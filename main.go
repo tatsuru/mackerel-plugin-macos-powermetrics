f1a177fa75a3d286e2b60eae3bdf819be15fecd4
wtatsuru <wtatsuru@hatena.ne.jp>
2021-12-05 03:16:19 +0900

    add per-CPU residency

--- main.go
+++ main.go
@@ -22,23 +22,39 @@ func (m MacosPowermetricsPlugin) GraphDefinition() map[string]mp.Graphs {
 	if err != nil {
 		return nil
 	}
-	metrics := []mp.Metrics{}
 
-	// frequency metrics
+	// CPU frequency metrics
+	freq_metrics := []mp.Metrics{}
 	for key, _ := range stat {
 		re := regexp.MustCompile(`frequency.(cpu[0-9]+)`)
 		res := re.FindStringSubmatch(key)
 		if res != nil {
 			// 	{Name: "frequency.cpu0", Label: "CPU0 Frequency MHz", Diff: false},
-			metrics = append(metrics, mp.Metrics{Name: key, Label: fmt.Sprintf("%s Frequency MHz", strings.ToUpper(res[1]))})
+			freq_metrics = append(freq_metrics, mp.Metrics{Name: key, Label: fmt.Sprintf("%s Frequency MHz", strings.ToUpper(res[1]))})
+		}
+	}
+
+	// CPU residency metrics
+	res_metrics := []mp.Metrics{}
+	for key, _ := range stat {
+		re := regexp.MustCompile(`residency.(cpu[0-9]+)`)
+		res := re.FindStringSubmatch(key)
+		if res != nil {
+			// 	{Name: "residency.cpu0", Label: "CPU0 Residency", Diff: false},
+			res_metrics = append(res_metrics, mp.Metrics{Name: key, Label: fmt.Sprintf("%s Residency", strings.ToUpper(res[1]))})
 		}
 	}
 
 	return map[string]mp.Graphs{
-		"": {
+		"frequency": {
 			Label:   labelPrefix + " CPU Frequency",
 			Unit:    mp.UnitInteger,
-			Metrics: metrics,
+			Metrics: freq_metrics,
+		},
+		"residency": {
+			Label:   labelPrefix + " CPU Residency %",
+			Unit:    mp.UnitPercentage,
+			Metrics: res_metrics,
 		},
 	}
 }
@@ -74,6 +90,19 @@ func parsePowermetrics(output string) (map[string]float64, error) {
 			k := fmt.Sprintf("frequency.cpu%s", res[1])
 			ret[k] = freq
 		}
+
+		// CPU residency
+		re = regexp.MustCompile(`^CPU ([0-9]+) active residency: +([0-9\.]+)%`)
+		res = re.FindStringSubmatch(line)
+		if res != nil {
+			residency, err := strconv.ParseFloat(res[2], 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse CPU residency: %s", err)
+			}
+			k := fmt.Sprintf("residency.cpu%s", res[1])
+			ret[k] = residency
+		}
+
 	}
 	return ret, nil
 }
